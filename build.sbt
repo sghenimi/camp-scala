fa4b770857c8cf2333048489d1e709c6a5ef32c1
sghenimi <soufiane.ghenimi@gmail.com>
2020-04-28 17:09:28 +0200

    count of word "val" in the source code

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "HelloSpark"
+
+version := "0.1"
+
+scalaVersion := "2.12.11"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.5"
+libraryDependencies += "org.apache.logging.log4j" % "log4j-core" % "2.4.1"
---
+++ src/main/scala/org/persocomp/tutoriel1/DemoCount.scala
@@ -0,0 +1,15 @@
+package org.persocomp.tutoriel1
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object DemoCount {
+  def main(args: Array[String]): Unit ={
+    val myFile = "src/main/scala/org/persocomp/tutoriel1/DemoCount.scala"
+    val conf = new SparkConf().setAppName("Hello Spark").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val textFileFile = sc.textFile(myFile,2).cache()
+    val numAs = textFileFile.filter(line => line.contains("val")).count()
+    println("#### Hello Spark")
+    println("#### Lines with word val: %s".format(numAs))
+  }
+}
